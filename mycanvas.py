e4cc6a02ccd18a798787e806c2517540922af675
Victor Filgueira <victor.filgueira@mindsatwork.com.br>
2023-09-25 23:49:53 -0300

    modeladorGraficoAula05

--- mycanvas.py
+++ mycanvas.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtOpenGL
 from PyQt5.QtWidgets import *
 from OpenGL.GL import *
+from PyQt5 import QtCore
 class MyCanvas(QtOpenGL.QGLWidget):
     
     def __init__(self):
@@ -13,17 +14,20 @@ class MyCanvas(QtOpenGL.QGLWidget):
         self.m_B = -1000.0
         self.m_T = 1000.0
         self.list = None
-
+        self.m_buttonPressed = False
+        self.m_pt0 = QtCore.QPoint(0, 0)
+        self.m_pt1 = QtCore.QPoint(0, 0)
+        
     def initializeGL(self):
         #glClearColor(1.0, 1.0, 1.0, 1.0)
         glClear(GL_COLOR_BUFFER_BIT)
+        glEnable(GL_LINE_SMOOTH)
         self.list = glGenLists(1)
-
+        
     def resizeGL(self, _width, _height):
         self.m_w = _width
         self.m_h = _height
-        if(self.m_model==None)or(self.m_model.isEmpty()): 
-            self.scaleWorldWindow(1.0)
+        if(self.m_model==None)or(self.m_model.isEmpty()): self.scaleWorldWindow(1.0)
         else:
             self.m_L,self.m_R,self.m_B,self.m_T = self.m_model.getBoundBox()
             self.scaleWorldWindow(1.1)
@@ -33,21 +37,66 @@ class MyCanvas(QtOpenGL.QGLWidget):
         glOrtho(self.m_L,self.m_R,self.m_B,self.m_T,-1.0,1.0)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-
+        
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT)
-        if(self.m_model==None)or(self.m_model.isEmpty()): return
+        if(self.m_model==None)or(self.m_model.isEmpty()): 
+            return
         glCallList(self.list)
         glDeleteLists(self.list, 1)
         self.list = glGenLists(1)
         glNewList(self.list, GL_COMPILE)
-        verts = self.m_model.getVerts()
-        glColor3f(0.0, 1.0, 0.0) # green
-        glBegin(GL_TRIANGLES)
-        for vtx in verts:
-            glVertex2f(vtx.getX(), vtx.getY())
+        pt0_U = self.convertPtCoordsToUniverse(self.m_pt0)
+        pt1_U = self.convertPtCoordsToUniverse(self.m_pt1)
+        glColor3f(1.0, 0.0, 0.0)
+        glBegin(GL_LINE_STRIP)
+        glVertex2f(pt0_U.x(), pt0_U.y())
+        glVertex2f(pt1_U.x(), pt1_U.y())
         glEnd()
+        if not((self.m_model == None) and (self.m_model.isEmpty())):
+            verts = self.m_model.getVerts()
+            glColor3f(0.0, 1.0, 0.0) # green
+            glBegin(GL_TRIANGLES)
+            for vtx in verts:
+                glVertex2f(vtx.getX(), vtx.getY())
+            glEnd()
+            curves = self.m_model.getCurves()
+            glColor3f(0.0, 0.0, 1.0) # blue
+            glBegin(GL_LINES)
+            for curv in curves:
+                glVertex2f(curv.getP1().getX(), curv.getP1().getY())
+                glVertex2f(curv.getP2().getX(), curv.getP2().getY())
+            glEnd()
         glEndList()
+        
+    def convertPtCoordsToUniverse(self, _pt):
+        dX = self.m_R - self.m_L
+        dY = self.m_T - self.m_B
+        mX = _pt.x() * dX / self.m_w
+        mY = (self.m_h - _pt.y()) * dY / self.m_h
+        x = self.m_L + mX
+        y = self.m_B + mY
+        return QtCore.QPointF(x,y)
+    
+    def mousePressEvent(self, event):
+        self.m_buttonPressed = True
+        self.m_pt0 = event.pos()
+    
+    def mouseMoveEvent(self, event):
+        if self.m_buttonPressed:
+            self.m_pt1 = event.pos()
+            self.update()
+        
+    def mouseReleaseEvent(self, event):
+        pt0_U = self.convertPtCoordsToUniverse(self.m_pt0)
+        pt1_U = self.convertPtCoordsToUniverse(self.m_pt1)
+        self.m_model.setCurve(pt0_U.x(), pt0_U.y(), pt1_U.x(), pt1_U.y())
+        #self.m_model.setCurve(self.m_pt0.x(),...,...,...)
+        self.m_buttonPressed = False
+        self.m_pt0.setX(0)
+        self.m_pt0.setY(0)
+        self.m_pt1.setX(0)
+        self.m_pt1.setY(0)
 
     def setModel(self,_model):
         self.m_model = _model
